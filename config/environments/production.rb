05b8baf1ecc01b25d712d66b7b0861b0a9721d81
Bob Fly <bob.fly.fanfo@gmail.com>
2018-11-28 19:13:21 +0100

    added more settings, translation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,26 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  ActionMailer::Base.smtp_settings = {
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: 'bandnotifier.herokuapp.com' }
+
+  config.mailer_sender = 'no-reply@bandnotifier.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'bandnotifier.heroku.com',
+  :domain         => 'bandnotifier.herokuapp.com',
   :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
