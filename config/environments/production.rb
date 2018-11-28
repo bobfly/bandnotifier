2e569443bc10f57e33c14c8b04722098715d4527
Bob Fly <bob.fly.fanfo@gmail.com>
2018-11-28 22:03:21 +0100

    changed to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,12 +81,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'bandnotifier.herokuapp.com',
-  :authentication => :plain,
+    :port           => 587,
+    :address        => 'smtp.sendgrid.net',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'bandnotifier.herokuapp.com',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
   }
 
 
